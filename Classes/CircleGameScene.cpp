e19f3c369d5701007cb91a306d1240eaeb2603e9
fgnoe <fgnoee@gmail.com>
2017-06-16 15:11:29 -0600

    Added circle to the CircleGameScene

--- Classes/CircleGameScene.cpp
+++ Classes/CircleGameScene.cpp
@@ -16,6 +16,21 @@ bool CircleGame::init()
     
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    Vec2 center = Vec2(origin.x + visibleSize.width/2,
+                       origin.y + visibleSize.height/2);
     
+    Sprite* circle = Sprite::create("sprites/circle_white.png");
+    circle->setPosition(center);
+    this->addChild(circle);
+    
+    ScaleTo* makeBigger = ScaleTo::create(0.25, 1.05);
+    ScaleTo* makeSmaller = ScaleTo::create(0.25, 0.95);
+    EaseSineInOut* easeBigger = EaseSineInOut::create(makeBigger);
+    EaseSineInOut* easeSmaller = EaseSineInOut::create(makeSmaller);
+    Sequence* sequenceAnimation = Sequence::create(easeBigger,easeSmaller, NULL);
+    RepeatForever* repeatAnimation = RepeatForever::create(sequenceAnimation);
+    
+    circle->runAction(repeatAnimation);
+
     return true;
 }
